cfe41d4a33f4d61f06bd1bd074fcde1bb532f380
Yassmin Ezzat <yassmin2104022@miuegypt.edu.eg>
2023-10-09 05:23:40 +0300

    Added JavaScript functions to open and close the nav.

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function openNavbar() {
+
+    document.getElementById("verNav").style.display = "block";
+    document.getElementById("open").style.display = "none";
+    document.getElementById("close").style.display = "block";
+
+}
+
+
+function closeNavbar(){
+
+    document.getElementById("verNav").style.display = "none";
+    document.getElementById("open").style.display = "block";
+    document.getElementById("close").style.display = "none";
+    
+}
