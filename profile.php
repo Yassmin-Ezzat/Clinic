796a6e2e4f1c8a1f45e82c0e1d12d62a172a06d5
Yassmin Ezzat <yassmin2104022@miuegypt.edu.eg>
2023-10-21 20:29:01 +0300

    Created the profile page.

---
+++ profile.php
@@ -0,0 +1,66 @@
+<?php
+include 'partials/nav.php'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/profile.css">
+    <title>Profile</title>
+</head>
+<body>
+<div class="MAIN">
+        <div class="signup">
+          <h1 class="sign">Welcome</h1>
+      </div>
+<div class="all">
+      <div class="info">
+        <p>Name: </p>
+        <p>Email: </p>
+        <p>Address: </p>
+      </div>
+        
+      <div class="right-nav">
+             <nav class="profile-nav">
+              <ul>
+                <li>
+                  <a href="index.php" >
+                  <i class="fa-solid fa-house" style="color: #1f1fb8;"></i>
+                  Dashboard
+                  </a>
+
+                <li>
+                  <a href="/user/wishlist">
+                  <i class="fa-regular fa-calendar-check" style="color: #1f1fb8;"></i>
+                  Register
+                  </a>
+                </li>
+                
+                <li>
+                  <a href="/user/wishlist">
+                  <i class="fa-solid fa-clock-rotate-left" style="color: #1f1fb8;"></i>
+                  History
+                  </a>
+                </li>
+
+                <li>
+                  <a href="editProfile.php">
+                    <i class="fa-regular fa-pen-to-square" style="color: #1f1fb8;"></i>
+                    Edit Profile
+                  </a>
+                </li>
+
+                <li>
+                  <a href="logOut.php">
+                  <i class="fa-solid fa-arrow-right-from-bracket" style="color: #1f1fb8;"></i>
+                    Logout</a>
+                </li>
+              </ul>
+             </nav>
+          </div>
+        </div>
+        </div>
+</body>
+</html>
