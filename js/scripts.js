804c574932def49e061bc4ad2b2b161a2588e43d
Raghda Elkhatib <125472396+Raghdakk@users.noreply.github.com>
2023-10-21 18:49:24 +0300

    doctorcss

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+document.getElementById('cardiology').addEventListener('click', function() {
+    alert('Cardiology is the branch of medicine that deals with disorders of the heart.');
+});
+
+document.getElementById('dermatology').addEventListener('click', function() {
+    alert('Dermatology is the branch of medicine dealing with the skin, nails, hair and its diseases.');
+});
+
+document.getElementById('orthopedics').addEventListener('click', function() {
+    alert('Orthopedics is the branch of surgery concerned with conditions involving the musculoskeletal system.');
+});
+
+document.getElementById('neurology').addEventListener('click', function() {
+    alert('Neurology is the branch of medicine that deals with disorders of the nervous system.');
+});
+document.getElementById('Endocrinology').addEventListener('click', function() {
+    alert('Neurology is the branch of medicine that deals with disorders of the nervous system.');
+});
+document.getElementById('ENT').addEventListener('click', function() {
+    alert('Neurology is the branch of medicine that deals with disorders of the nervous system.');
+});
+document.getElementById('Nutrition').addEventListener('click', function() {
+    alert('Neurology is the branch of medicine that deals with disorders of the nervous system.');
+});
+document.getElementById('PediatricSurgery').addEventListener('click', function() {
+    alert('Neurology is the branch of medicine that deals with disorders of the nervous system.');
+});
---
+++ specialties.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Medical Specialties</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <div class="box" id="cardiology">
+            <h2>Cardiology</h2>
+            <p>The branch of medicine that deals with disorders of the heart.</p>
+        </div>
+        <div class="box" id="dermatology">
+            <h2>Dermatology</h2>
+            <p>The branch of medicine dealing with the skin, nails, hair and its diseases.</p>
+        </div>
+        <div class="box" id="orthopedics">
+            <h2>Orthopedics</h2>
+            <p>The branch of surgery concerned with conditions involving the musculoskeletal system.</p>
+        </div>
+        <div class="box" id="neurology">
+            <h2>Neurology</h2>
+            <p>The branch of medicine that deals with disorders of the nervous system.</p>
+        </div>
+        <div class="box" id="Endocrinology">
+            <h2>Endocrinology</h2>
+            <p>The branch of medicine that deals with disorders of the nervous system.</p>
+        </div>
+        <div class="box" id="ENT">
+            <h2>ENT</h2>
+            <p>The branch of medicine that deals with disorders of the nervous system.</p>
+        </div>
+        <div class="box" id="Nutrition">
+            <h2>Nutrition</h2>
+            <p>The branch of medicine that deals with disorders of the nervous system.</p>
+        </div>
+        <div class="box" id="PediatricSurgery">
+            <h2>PediatricSurgery</h2>
+            <p>The branch of medicine that deals with disorders of the nervous system.</p>
+            </div>
+    </div>
+    <script src="scripts.js"></script>
+</body>
+</html>
