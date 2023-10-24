a1740632ff9e87c671dbfb578973887c8ee05a1c
Abdallah Hamdy <96912938+Abdallah2110678@users.noreply.github.com>
2023-10-24 13:56:44 +0300

    add the signup handler (frontend)

--- PHP/about.php
+++ PHP/about.php
@@ -1,24 +1,80 @@
 <?php
-include 'partials/nav.php'
-?>
+    include "../partials/nav.php";
+    ?>
 
-<!DOCTYPE html>
-<html lang="en">
+ <!DOCTYPE html>
+ <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About us</title>
-    <link rel="stylesheet" href="css/about.css">
-</head>
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>About us</title>
+     <link rel="stylesheet" href="../public/css/about.css">
+ </head>
 
-<body>
-    <div class="container">
-        <header>
-            <h1>Welcome to Our Hospital</h1>
-        </header>
+ <body>
 
-        <section class="about-section">
+     <div class="heading">
+         <h1>About Us</h1>
+         <p>Our hospital is more than just a medical facility; it's a place of healing and comfort.
+             We are dedicated to providing the highest quality healthcare services to our patients,
+             ensuring their well-being and satisfaction. Our experienced team of healthcare professionals
+             is committed to delivering personalized care and making a positive difference in the community.
+         </p>
+     </div>
+     <div class="container">
+         <section class="about">
+             <div class="about_img">
+                 <img src="../public/images\1670257570pic-13.jpg">
+             </div>
+             <div class="about_content">
+                 <h2>Contact Information</h2>
+                 <p>
+                     <strong>Address:</strong><br>
+                     ######
+                 </p>
+                 <p>
+                     <strong>Email:</strong> <br>
+                     <strong>Phone:</strong>15292
+                 </p>
+                 <a href=""class="read-more">Read More</a>
+             </div>
+         </section>
+     </div>
+     <br>
+     <section class="mission-section">
+            <div class="content">
+                <h2>Our Mission</h2>
+                <p>
+                    Our mission is to enhance the health and well-being of the communities we serve
+                    by providing accessible, high-quality healthcare services. We strive to promote a
+                    culture of compassion, innovation, and excellence in patient care.
+                </p>
+            </div>
+        </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     <!-- <div class="container">
+         <section class="about-section">
             <div class="content">
                 <h2>About Us</h2>
                 <p>
@@ -54,7 +110,11 @@ include 'partials/nav.php'
                 </p>
             </div>
         </section>
-    </div>
-</body>
+    </div> -->
+     <?php
+        include '../partials/footer.php';
+        ?>
+ </body>
 
-</html>
+
+ </html>
