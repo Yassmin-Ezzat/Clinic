542ea236c97ad112dfa6e11fba5986eca9d18db7
Abdallah Hamdy <96912938+Abdallah2110678@users.noreply.github.com>
2023-10-26 04:45:24 +0300

    updated the dashboard

--- PHP/dashboard.php
+++ PHP/dashboard.php
@@ -1,3 +1,6 @@
+<?php
+include "../PHP/sidebar.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,105 +9,57 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 
     <link rel="stylesheet" href="../public/css/dashboard.css">
 </head>
 
 <body>
-    <!-- =============== Navigation ================ -->
-    <div class="container">
-        <div class="navigation">
-            <ul>
-                <li>
-                    <a href="../PHP/index.php">
-                        <span class="icon">
-                            <ion-icon name="logo.png"></ion-icon>
-                        </span>
-                        <span class="title">ICare</span>
-                    </a>
-                </li>
+    <div class="cardBox">
+        <div class="card">
+            <div>
+                <div class="numbers">0</div>
+                <div class="cardName">Appointment Today</div>
+            </div>
 
-                <li>
-                    <a href="../PHP/dashboard.php">
-                        <span class="icon">
-                            <ion-icon name="home-outline"></ion-icon>
-                        </span>
-                        <span class="title">Dashboard</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="../PHP/addnewdoctor.php">
-                        <span class="icon">
-                            <ion-icon name="add-outline"></ion-icon>
-                        </span>
-                        <span class="title">Add Doctors</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="edit.php">
-                        <span class="icon">
-                            <ion-icon name="create-outline"></ion-icon>
-                        </span>
-                        <span class="title">Edit</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="delete.php">
-                        <span class="icon">
-                            <ion-icon name="trash-outline"></ion-icon>
-                        </span>
-                        <span class="title">Delete</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="read.php">
-                        <span class="icon">
-                            <ion-icon name="people-outline"></ion-icon>
-                        </span>
-                        <span class="title">Doctors</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="#">
-                        <span class="icon">
-                            <ion-icon name="lock-closed-outline"></ion-icon>
-                        </span>
-                        <span class="title">Password</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="#">
-                        <span class="icon">
-                            <ion-icon name="log-out-outline"></ion-icon>
-                        </span>
-                        <span class="title">Sign Out</span>
-                    </a>
-                </li>
-            </ul>
+            <div class="iconBx">
+                <ion-icon name="schedule-outline"></ion-icon>
+            </div>
         </div>
-        <div class="main">
-            <div class="topbar">
-                <div class="toggle">
-                    <ion-icon name="menu-outline"></ion-icon>
-                </div>
-                <div class="user">
-                    <h2>hi,admin</h2>
-                </div>
+
+        <div class="card">
+            <div>
+                <div class="numbers">80</div>
+                <div class="cardName">Total patients</div>
+            </div>
+
+            <div class="iconBx">
+                <ion-icon name="people-outline"></ion-icon>
+            </div>
+        </div>
+
+        <div class="card">
+            <div>
+                <div class="numbers">284</div>
+                <div class="cardName">Messages</div>
+            </div>
+
+            <div class="iconBx">
+                <ion-icon name="chatbubbles-outline"></ion-icon>
+            </div>
+        </div>
+
+        <div class="card">
+            <div>
+                <div class="numbers">$7,842</div>
+                <div class="cardName">Earning</div>
+            </div>
+
+            <div class="iconBx">
+                <ion-icon name="cash-outline"></ion-icon>
             </div>
         </div>
     </div>
 
-    <script src="../public/js/dashboard.js"></script>
-
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
 
 </html>
