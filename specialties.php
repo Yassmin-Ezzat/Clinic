bef58b7bfb337639e4db5e42c1df8c77ce4e880a
Raghda Elkhatib <125472396+Raghdakk@users.noreply.github.com>
2023-11-20 10:07:08 +0200

    edit

--- specialties.php
+++ specialties.php
@@ -10,34 +10,42 @@
 <body>
     <div class="container">
         <div class="box" id="cardiology">
+        <img  class=spec1p src="images/card.png">
             <h2>Cardiology</h2>
             <p>The branch of medicine that deals with disorders of the heart.</p>
         </div>
         <div class="box" id="dermatology">
+        <img  class=spec2p src="images/dermatology.png">
             <h2>Dermatology</h2>
             <p>The branch of medicine dealing with the skin, nails, hair and its diseases.</p>
         </div>
         <div class="box" id="orthopedics">
+        <img  class=spec3p src="images/ortho.png">
             <h2>Orthopedics</h2>
             <p>The branch of surgery concerned with conditions involving the musculoskeletal system.</p>
         </div>
         <div class="box" id="neurology">
+        <img  class=spec4p src="images/neurology.png">
             <h2>Neurology</h2>
             <p>The branch of medicine that deals with disorders of the nervous system.</p>
         </div>
         <div class="box" id="Endocrinology">
+        <img  class=spec5p src="images/endo.png">
             <h2>Endocrinology</h2>
             <p>Endocrinology is a branch of biology and medicine dealing with the endocrine system, its diseases, and its specific secretions known as hormones..</p>
         </div>
         <div class="box" id="ENT">
+        <img  class=spec6p src="images/ent.png">
             <h2>ENT</h2>
             <p>Ear, nose and throat.</p>
         </div>
         <div class="box" id="Nutrition">
+        <img  class=spec7p src="images/nutrition.png">
             <h2>Nutrition</h2>
             <p>Weight loss and gain.</p>
         </div>
         <div class="box" id="PediatricSurgery">
+        <img  class=spec8p src="images/pedia.png">
             <h2>PediatricSurgery</h2>
             <p>Pediatric surgery is a subspecialty of surgery involving the surgery of fetuses, infants, children, adolescents, and young adults.</p>
             </div>
