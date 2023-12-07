269dcf3e98aa6306d8754550d91abd3419e92e4d
Yassmin Ezzat <yassmin2104022@miuegypt.edu.eg>
2023-12-07 03:56:39 +0200

    Updated the edit profile form with paths and sessions.

--- view/editProfile.php
+++ view/editProfile.php
@@ -18,24 +18,23 @@ include "../partials/nav.php";
     </div>
 
     <div class="sign">
-    <form name="sign-up" action="#" method="post" onsubmit="#">
-    <input id="fir" type="text" class="element" name="Firstname" value="<?php echo isset($_SESSION['Name']) ? $_SESSION['Name'] : ''; ?>"><br>
+    <form name="sign-up" action="../db/patient.php" method="post" onsubmit="return signupHandler(event)">
+        <input type="hidden" name="action" value="UpdateUser">
 
-    <input id="Age" type="number" class="element" name="Age" value="<?php echo isset($_SESSION['Age']) ? $_SESSION['Age'] : ''; ?>"><br>
+        <input id="fir" type="text" class="element" name="Firstname" value="<?php echo isset($_SESSION['Name']) ? $_SESSION['Name'] : ''; ?>"><br>
 
-    <input id="ema" type="email" class="element" name="email" value="<?php echo isset($_SESSION['Email']) ? $_SESSION['Email'] : ''; ?>"><br>
+        <input id="Age" type="number" class="element" name="Age" value="<?php echo isset($_SESSION['Age']) ? $_SESSION['Age'] : ''; ?>"><br>
 
-    <input id="add" type="text" class="element" name="address" value="<?php echo isset($_SESSION['Address']) ? $_SESSION['Address'] : ''; ?>"><br>
+        <input id="ema" type="email" class="element" name="email" value="<?php echo isset($_SESSION['Email']) ? $_SESSION['Email'] : ''; ?>"><br>
 
-    <input id="pa" type="text" class="element" name="pas" placeholder="Password" value="<?php echo isset($_SESSION['Password']) ? $_SESSION['Password'] :''; ?>"><br>
+        <input id="add" type="text" class="element" name="address" value="<?php echo isset($_SESSION['Address']) ? $_SESSION['Address'] : ''; ?>"><br>
 
-    <input id="pac" type="password" class="element" name="pasconfirm" placeholder="Confirm Password"><br>
+        <input id="pa" type="text" class="element" name="pas" placeholder="Password" value="<?php echo isset($_SESSION['Password']) ? $_SESSION['Password'] :''; ?>"><br>
 
-    <input class="submit" type="submit" value="Update"><br>
-    <input class="submit" type="submit" value="Back"><br>
-</form>
+        <input class="submit" type="submit" value="Update"><br>
+    </form>
+
+    <a style="text-decoration: none; display: inline-block;" href="../view/profile.php" class="submit">Back</a>
 </div>
-
-
 </body>
 </html>
