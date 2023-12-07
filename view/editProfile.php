88d8723d34ab404ec92bffac58a480ca4c02eda2
Yassmin Ezzat <yassmin2104022@miuegypt.edu.eg>
2023-12-07 03:23:53 +0200

    Updated the edit profile page to read from the session.

--- view/editProfile.php
+++ view/editProfile.php
@@ -1,5 +1,6 @@
-<?php 
-include 'partials/nav.php';
+<?php
+session_start();
+include "../partials/nav.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,22 +18,22 @@ include 'partials/nav.php';
     </div>
 
     <div class="sign">
-    <form name="sign-up" action="#" method="post" onsubmit="#">        
-        <input id="fir" type="text" class="element" name="Firstname" placeholder="First Name"><br>
+    <form name="sign-up" action="#" method="post" onsubmit="#">
+    <input id="fir" type="text" class="element" name="Firstname" value="<?php echo isset($_SESSION['Name']) ? $_SESSION['Name'] : ''; ?>"><br>
 
-        <input id="las" type="text" class="element" name="last" placeholder="Last Name"><br>
+    <input id="Age" type="number" class="element" name="Age" value="<?php echo isset($_SESSION['Age']) ? $_SESSION['Age'] : ''; ?>"><br>
 
-        <input id="ema" type="email" class="element" name="email" placeholder="Email"><br>
+    <input id="ema" type="email" class="element" name="email" value="<?php echo isset($_SESSION['Email']) ? $_SESSION['Email'] : ''; ?>"><br>
 
-        <input id="add" type="text" class="element" name="address" placeholder="Address"><br>
+    <input id="add" type="text" class="element" name="address" value="<?php echo isset($_SESSION['Address']) ? $_SESSION['Address'] : ''; ?>"><br>
 
-        <input id="pa" type="password" class="element" name="pas" placeholder="Password"><br>
+    <input id="pa" type="text" class="element" name="pas" placeholder="Password" value="<?php echo isset($_SESSION['Password']) ? $_SESSION['Password'] :''; ?>"><br>
 
-        <input id="pac" type="password" class="element" name="pasconfirm" placeholder="Confirm Password"><br>
+    <input id="pac" type="password" class="element" name="pasconfirm" placeholder="Confirm Password"><br>
 
-        <input class="submit" type="submit" value="Update"><br>
-        <input class="submit" type="submit" value="Back"><br>
-    </form>
+    <input class="submit" type="submit" value="Update"><br>
+    <input class="submit" type="submit" value="Back"><br>
+</form>
 </div>
 
 
