0417880560bebe9b628ecae2b781c678041ac476
Raghda Elkhatib <125472396+Raghdakk@users.noreply.github.com>
2023-12-13 17:34:04 +0200

    edit

--- view/specialties.php
+++ view/specialties.php
@@ -15,53 +15,66 @@ include "../partials/nav.php";
     <br><br><br> <br><br><br> <br><br><br>
     <div class="container">
         <a class=linedecor href="doctor details.php">
-            <div class="box" id="cardiology">
-                <h2>Cardiology</h2>
-                <p>The branch of medicine that deals with disorders of the heart.</p>
+            <div class="box" id="orthopedic">
+                <h2>Orthopedic</h2>
+                <p>Orthopedic surgeons are doctors who specialize in the musculoskeletal system</p>
             </div>
         </a>
         <a class=linedecor href="doctor details.php">
-            <div class="box" id="dermatology">
-                <h2>Dermatology</h2>
-                <p>The branch of medicine dealing with the skin, nails, hair and its diseases.</p>
+            <div class="box" id="pediatrician ">
+                <h2>Pediatrician </h2>
+                <p> A pediatrician is a doctor who treats children. 
+                    They provide: Preventive health maintenance and ongoing monitoring for healthy children.</p>
             </div>
         </a>
         <a class=linedecor href="doctor details.php">
-            <div class="box" id="orthopedics">
-                <h2>Orthopedics</h2>
-                <p>The branch of surgery concerned with conditions involving the musculoskeletal system.</p>
+            <div class="box" id="surgeon">
+                <h2>Surgeaon</h2>
+                <p>Surgeons operate on patients to treat injuries, 
+                    such as broken bones; diseases, such as cancerous tumors; and deformities, such as cleft palates.</p>
             </div>
         </a>
         <a class=linedecor href="doctor details.php">
-            <div class="box" id="neurology">
-                <h2>Neurology</h2>
-                <p>The branch of medicine that deals with disorders of the nervous system.</p>
+            <div class="box" id="urologist">
+                <h2>Urologist</h2>
+                <p>A urologist is a medical doctor specializing in conditions that affect the urinary tract
+                     in men, women and children, and diseases that affect the reproductive system</p>
             </div>
         </a>
         <a class=linedecor href="doctor details.php">
-            <div class="box" id="Endocrinology">
-                <h2>Endocrinology</h2>
-                <p>Endocrinology is a branch of biology and medicine dealing with the endocrine system, its diseases, and its specific secretions known as hormones..</p>
+            <div class="box" id="radation oncologist">
+                <h2> Radation oncologist</h2>
+                <p>If your cancer can be treated with radiation, you will be referred to a radiation oncologist
+                     — a doctor who specializes in treating patients with radiation therapy</p>
             </div>
         </a>
         <a class=linedecor href="doctor details.php">
-            <div class="box" id="ENT">
-                <h2>ENT</h2>
-                <p>Ear, nose and throat.</p>
+            <div class="box" id="Ultrasonography ">
+                <h2>Ultrasonography</h2>
+                <p>An ultrasound is an imaging test that uses sound waves to make pictures of organs,
+                     tissues, and other structures inside your body</p>
             </div>
         </a>
         <a class=linedecor href="doctor details.php">
-            <div class="box" id="Nutrition">
-                <h2>Nutrition</h2>
-                <p>Weight loss and gain.</p>
+            <div class="box" id="Obstetrics and Gynecology">
+                <h2>Obstetrics and Gynecology </h2>
+                <p>Obstetrics and gynecology is a broad and diverse branch of medicine,
+                     including surgery, management of the care of pregnant women,</p>
             </div>
         </a>
         <a class=linedecor href="doctor details.php">
-            <div class="box" id="PediatricSurgery">
-                <h2>PediatricSurgery</h2>
-                <p>Pediatric surgery is a subspecialty of surgery involving the surgery of fetuses, infants, children, adolescents, and young adults.</p>
+            <div class="box" id="Vascular">
+                <h2>Vascular </h2>
+                <p>Vascular surgeons are specialists who are highly trained to treat diseases of the vascular system</p>
             </div>
         </a>
+        <a class=linedecor href="doctor details.php">
+            <div class="box" id="plastic surgeon">
+                <h2>Plastic surgeon</h2>
+                <p>Plastic surgery restores and improves function, as well as appearance.</p>
+            </div>
+        </a>
+   
     </div>
     <script src="scripts.js"></script>
 </body>
