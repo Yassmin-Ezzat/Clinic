b702b1bb2b999b4c3e687fe3ad2f1dcaf359a8f7
Reem omar <125567674+reemalbasset@users.noreply.github.com>
2023-12-20 06:42:27 +0200

    appointment review backend

--- view/appointmentreview.php
+++ view/appointmentreview.php
@@ -1,58 +1,88 @@
 <?php
 include "../partials/nav.php";
+include '..\db\dbh.inc.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../public/css/appointmentreview.css">
-  <title>appointment review</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/css/appointmentreview.css">
+    <title>appointment review</title>
 </head>
-
 <body>
-  <h1>Welcome User</h1>
-  <br><br><br>
+  <?php
+session_start();
 
-  <table id="customers">
-
-    <tr>
-      <th>Available Appointments</th>
-    </tr>
-
-  </table>
-  <br>
-
-  <table id="customer">
-
-    <tr>
-      <td>Adam Yehia</td>
-      <td>Eye-care</td>
-      <td>23/12/2023</td>
-      <td>8:00 AM</td>
-    </tr>
-    <tr>
-      <td>Laila Youssef</td>
-      <td>Pediatrics</td>
-      <td>10/10/2024</td>
-      <td>6:00 PM</td>
-    </tr>
-    <tr>
-      <td>Ahmed sedky</td>
-      <td>Internal Medicine</td>
-      <td>14/06/2023</td>
-      <td>10:00 AM</td>
-    </tr>
-  </table>
+// Check if success flag is present
+if (isset($_GET['success']) && $_GET['success'] === 'true') {
+    // Display success message
+    echo "<h1>Appointment booked successfully</h1>";
+}
 
 
 
+?>
+<h1>Welcome <?php echo $_SESSION["Name"]; ?></h1>
+<br><br><br>
 
+<table id="customers">
 
+  <tr>
+    <th>Available Appointments</th>
+  </tr>
+
+</table>
+<br> 
+<?php
+// Fetch upcoming appointments with doctor and patient details from the 'appointment' table
+include_once "../db/dbh.inc.php";
+if (isset($_SESSION['ID'])) {
+  $patient_id = $_SESSION['ID'];
+  
+
+$sql = "SELECT appointment.Appointment_id,  patient.name AS patient_name, 
+        appointment.date, appointment.time 
+        FROM appointment 
+        INNER JOIN doctor ON appointment.doctor_id = doctor.id 
+        INNER JOIN patient ON appointment.patient_id = patient.id 
+        WHERE appointment.date > NOW() 
+        ORDER BY appointment.date, appointment.time";
+
+$result = mysqli_query($connection, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    echo "<table id=customer>
+            <tr>
+                <th>Appointment ID</th>
+                <th>Patient</th>
+                <th>Appointment Date</th>
+                <th>Appointment Time</th>
+            </tr>";
+            $query = "SELECT * FROM appointment WHERE patient_id = '" . $_SESSION['ID'] . "'";
+            $result = mysqli_query($connection, $query);
+    while ($row = mysqli_fetch_assoc($result)) {
+      $formattedTime = isset($row['time']) ?date("h:i A", strtotime($row['time'])):'';
+        echo "<tr>
+                <td>{$row['Appointment_id']}</td>
+                <td>{$_SESSION['Name']}</td>
+                <td>{$row['Date']}</td>
+                <td>{$formattedTime}</td>
+              </tr>";
+    }
+
+   } echo "</table>";
+} else {
+    echo "<p>No upcoming appointments.</p>";
+}
+
+$connection->close();
+
+  
+
+?>
   <br><br><br><br><br><br><br><br><br><br><br>
 </body>
-
 </html>
 <?php
 include '..\partials\footer.php';
